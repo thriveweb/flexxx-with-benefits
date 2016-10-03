cbda3431b5840ec67ba7774a5a71f47f358c40c8
Dean Oakley <dean@thriveweb.com.au>
2016-10-03 16:08:42 +1000

    - + buttons not worky

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 jQuery(document).ready(function($) {
 
 	(function plusMinus(){
-		$(".plusminusbutton").click( function(){
-			var $button = $(this);
-			var oldValue = $button.parent().find(".input-text.qty").val();
-			var newVal;
+		$(document).on('click', '.plusminusbutton', function(){
+			var $button = $(this)
+			var oldValue = $button.parent().find('.input-text.qty').val()
+			var newVal
 			if ( $button.hasClass('plus') ) {
-				newVal = parseFloat(oldValue) + 1;
+				newVal = parseFloat(oldValue) + 1
 			} else {
 				// Don't allow decrementing below zero
 				if (oldValue > 0) {
-					newVal = parseFloat(oldValue) - 1;
+					newVal = parseFloat(oldValue) - 1
 				} else {
-					newVal = 0;
+					newVal = 0
 				}
 			}
-			$button.parent().find(".input-text.qty").val(newVal);
-		});
+			$button.parent().find('.input-text.qty').val(newVal).change()
+		})
 	})();
 
 	(function clearSearchOnClick(){
